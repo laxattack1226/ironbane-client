9fb900bb0c55fdf7670c9dc710b197651f317e2f
Nick Janssen <info@nickjanssen.com>
2014-08-17 18:15:59 +0200

    working with angus 0.3

--- angus.config.js
+++ angus.config.js
@@ -2,46 +2,36 @@
 
 module.exports = {
 
-    packages: [
-        'angular',
-        'http://cdnjs.cloudflare.com/ajax/libs/three.js/r68/three.js',
-        'https://github.com/dmauro/Keypress/archive/2.0.3.tar.gz',
-        'underscore',
-        'angular-ui-router',
-        'hammerjs'
-    ],
-
-    port: 9000,
-
-    libIncludes: {
-        js: [
-            'angular/angular.js',
-            'angular-ui-router/release/angular-ui-router.js',
-
-            'Keypress/keypress.js',
-            'hammerjs/hammer.min.js',
-
-            // Bug in bower makes us read it as index.js
-            // https://github.com/bower/bower/issues/561
-            'three/index.js',
-
-            'underscore/underscore.js'
+    bower: {
+        packages: [
+            'angular',
+            'http://cdnjs.cloudflare.com/ajax/libs/three.js/r68/three.js',
+            'https://github.com/dmauro/Keypress/archive/2.0.3.tar.gz',
+            'underscore',
+            'angular-ui-router',
+            'hammerjs'
         ],
 
-        tpl: [],
+        filesNeeded: {
+            js: [
+                'angular/angular.js',
+                'angular-ui-router/release/angular-ui-router.js',
 
-        scss: []
+                'Keypress/keypress.js',
+                'hammerjs/hammer.min.js',
+
+                // Bug in bower makes us read it as index.js
+                // https://github.com/bower/bower/issues/561
+                'three/index.js',
+
+                'underscore/underscore.js'
+            ]
+        }
     },
 
-    // In addition to the default task list (core/defaultTasks.js), also execute these
-    gruntTasksAdd: [
-        'html2js',
-        'ngconstant',
-        'ngmin',
-    ],
+    usesAngularJS: true,
 
-    gruntTasksIgnore: ['sass', 'sass_import_compiler'],
+    testRunner: 'none',
 
-    // ngconstant will parse this object and allow you to access them in your app
-    constants: {}
+    cssCompiler: 'less'
 };
