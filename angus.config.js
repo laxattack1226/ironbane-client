a3bb94a50101745d43ac0d6f7ce286514b23327c
Ben Sparks <bsparks42@gmail.com>
2014-09-04 08:31:05 -0700

    switch to using bower package for threejs and Keypress

--- angus.config.js
+++ angus.config.js
@@ -5,8 +5,8 @@ module.exports = {
     bower: {
         packages: [
             'angular',
-            'http://cdnjs.cloudflare.com/ajax/libs/three.js/r68/three.js',
-            'https://github.com/dmauro/Keypress/archive/2.0.3.tar.gz',
+            'threejs',
+            'Keypress',
             'underscore',
             'angular-ui-router',
             'hammerjs'
@@ -16,14 +16,9 @@ module.exports = {
             js: [
                 'angular/angular.js',
                 'angular-ui-router/release/angular-ui-router.js',
-
                 'Keypress/keypress.js',
                 'hammerjs/hammer.min.js',
-
-                // Bug in bower makes us read it as index.js
-                // https://github.com/bower/bower/issues/561
-                'three/index.js',
-
+                'threejs/build/three.js',
                 'underscore/underscore.js'
             ]
         }
